83042c973509cdab95f6c9b1754222db7461d739
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-10-07 14:19:55 -0700

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
 export function managePresents(state, action){
+    switch(action.type) {
+        case 'INCREASE':
+            return { ...state,
+                numberOfPresents: state.numberOfPresents + 1 
+            }
+        default:
+            return state;
+    }
 }
+let state = {managePresents: 0}
+
+managePresents(state, {type: 'INCREASE'})
